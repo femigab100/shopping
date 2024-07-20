31954bb61f2be482382d3e18fc4b57a989512f51
femigab100 <femigab100@gmail.com>
2024-07-20 19:37:54 +0100

    First commit of my React Shopping and Carting App

---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,20 @@
+import {PRODUCTS} from '../../products'
+import {Product} from './Product'
+import './shop.css'
+
+export const Shop = () => {
+  return (
+    <div className='shop'>
+
+        <div className='shopTitle'>
+            <h1> Desaint Tech Shop</h1>
+        </div>
+        <div className='products'>
+            {" "}
+            {PRODUCTS.map((product) => ( 
+                <Product data={product}/> ))}
+        </div>
+    </div>
+  )
+}
+
